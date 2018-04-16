d03bf1dab9f847add06d8129fcec0a16639cbe73
Pendzik <andrzej.stadnik@hotmail.com>
2018-04-16 12:42:33 +0200

    mod

---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+import { AuthGuard } from './_guards/index';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    // {path:'',component: HomepageComponent},
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    
+    
+    
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
