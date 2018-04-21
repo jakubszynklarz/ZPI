5d258abb1f714b2fc4d3f10eb8cbbdc1caea30c1
RFrankowski <r.franek93@gmail.com>
2018-04-21 17:59:44 +0200

    modyfikacja panelu oraz poprawa formatowania zegaru

--- src/app/panel-sedziowski/panel-sedziowski.component.ts
+++ src/app/panel-sedziowski/panel-sedziowski.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel-sedziowski.component.css']
 })
 export class PanelSedziowskiComponent implements OnInit {
+  
+  
+
+  //#region zmienne
   duzePunktyZawodnik1: number = 0;
   duzePunktyZawodnik2: number = 0;
   
@@ -18,7 +22,7 @@ export class PanelSedziowskiComponent implements OnInit {
   licznik = 0;
   flagaStart = 0;
   
-  ZegarCzas: string = "0";
+  ZegarCzas: string = "00:00";
   btnZegar:string = "start"
   //#endregion
   constructor() { }
@@ -30,22 +34,40 @@ export class PanelSedziowskiComponent implements OnInit {
   //#region  obslugaPunktow
   zwiekszDuzeZawodnik1(ilePunktow){
     this.duzePunktyZawodnik1 += ilePunktow;
+    if (this.duzePunktyZawodnik1 < 0){
+      this.duzePunktyZawodnik1 = 0;
+    }
   }
   zwiekszDuzeZawodnik2(ilePunktow){
     this.duzePunktyZawodnik2 += ilePunktow;
+    if (this.duzePunktyZawodnik2 < 0){
+      this.duzePunktyZawodnik2 = 0;
+    }
   }
 
   zwiekszKaryZawodnik1(ilePunktow){
     this.karyZawodnik1 += ilePunktow;
+    if (this.karyZawodnik1 > 0){
+      this.karyZawodnik1 = 0;
+    }
   }
   zwiekszKaryZawodnik2(ilePunktow){
     this.karyZawodnik2 += ilePunktow;
+    if (this.karyZawodnik2 > 0){
+      this.karyZawodnik2 = 0;
+    }
   }
   zwiekszPrzewagiZawodnik1(ilePunktow){
     this.przewagiZawodnik1 += ilePunktow;
+    if (this.przewagiZawodnik1 < 0){
+      this.przewagiZawodnik1 = 0;
+    }
   }
   zwiekszPrzewagiZawodnik2(ilePunktow){
     this.przewagiZawodnik2 += ilePunktow;
+    if (this.przewagiZawodnik2 < 0){
+      this.przewagiZawodnik2 = 0;
+    }
   }
   //#endregion 
   
@@ -54,13 +76,23 @@ export class PanelSedziowskiComponent implements OnInit {
     //console.log(flagaStart);
     if (this.flagaStart === 1) {
       this.licznik++;
-        // let sekundy = this.licznik % 60;
-        // let minuty = Math.floor(this.licznik / 60);
-        // wyswietlCzas(minuty, sekundy);
-        // setTimeout('zegar()',1000);
+        let sekundy = this.licznik % 60;
+        let minuty = Math.floor(this.licznik / 60);
+        this.wyswietlCzas(minuty, sekundy);
         setTimeout(()=>{ this.zegar() ;} ,1000);
     }
   }
+
+   wyswietlCzas(minuty:number, sekundy:number): any {
+    if (minuty < 10 && sekundy < 10) {
+        this.ZegarCzas = "0" + minuty + ":0" + sekundy;
+    }
+    else if (minuty < 10) {
+        this.ZegarCzas = "0" + minuty + ":" + sekundy;
+    } else {
+        this.ZegarCzas = + minuty + ":" + sekundy;
+    }
+}
 
   StartStop() {
 
@@ -78,5 +110,18 @@ export class PanelSedziowskiComponent implements OnInit {
       this.flagaStart = 0;
     }
   }
+
+   zresetujZegarIpunkty() {
+    this.flagaStart = 2;
+    this.StartStop();
+    this.licznik = 0;
+    this.ZegarCzas = "00:00";
+    this.duzePunktyZawodnik1 = 0;
+    this.duzePunktyZawodnik2 = 0;
+    this.przewagiZawodnik1 = 0;
+    this.przewagiZawodnik2 = 0;
+    this.karyZawodnik1 = 0;
+    this.karyZawodnik2 = 0;
+}
 
 }
