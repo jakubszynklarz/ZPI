062a523656ca584c573feeecb865f451cbb8c3ef
jakubszynklarz <jakubszynklarz@gmail.com>
2018-04-24 17:46:03 +0200

    'lista'

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  frangmentLoremIpsum: string = "Nullam in vestibulum lacus, id venenatis elit. Quisque sed commodo mauris. Nulla eleifend orci quis mi ornare, ac efficitur est condimentum.  "
+  frangmentLoremIpsum: string = "Nullam in vestibulum lacus, id venenatis elit.   "
   constructor() { }
 
   ngOnInit() {
