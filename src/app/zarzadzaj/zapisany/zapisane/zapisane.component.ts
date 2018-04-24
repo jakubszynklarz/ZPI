c03fbf06c77497edf66aa5036647ebc4d611b6f8
Pendzik <andrzej.stadnik@hotmail.com>
2018-04-24 08:59:23 +0200

    poprawa po zmianach

--- src/app/zarzadzaj/zapisany/zapisane/zapisane.component.ts
+++ src/app/zarzadzaj/zapisany/zapisane/zapisane.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Modeloo } from '../../shared/modeloo.model';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { Kategorie } from '../../shared/kategorie.model';
 
 @Component({
   selector: 'app-zapisane',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZapisaneComponent implements OnInit {
 
-  constructor() { }
+  public check:number;
+  public defula:number=0;
+  selectedModeloo: Modeloo = new Modeloo();
 
-  ngOnInit() {
+  public current = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
+  private turnieCollection: AngularFirestoreCollection<Modeloo>;
+
+  private turnieje: Observable<any[]>;
+  private kategorie: Observable<any[]>;
+  private kateCollection: AngularFirestoreCollection<Kategorie>;
+
+  
+  constructor(private db: AngularFirestore) { 
+    
+    this.turnieCollection=db.collection<Modeloo>('/turnieje');
+    this.turnieje=this.turnieCollection.snapshotChanges().map(actions =>{
+      return actions.map( a=>{
+        const data = a.payload.doc.data() as Modeloo;
+        const id = a.payload.doc.id;
+        
+        return {id,...data};
+      })
+    })
+
+    
+    
   }
+  ngOnInit() {
+   
+    
+   
+    
+    
+  }
+ 
+  private update(){
+    var updateNested = this.db.collection('turnieje').doc(this.current).update({
+      opis: this.selectedModeloo.opis,
+      kiedy:this.selectedModeloo.kiedy,
+      do_kiedyrej:this.selectedModeloo.do_kiedyrej,
+      miejsce:this.selectedModeloo.miejsce,
+      nazwa:this.selectedModeloo.nazwa,
+      nr_konta:this.selectedModeloo.nr_konta,
+      wpisowe:this.selectedModeloo.wpisowe
+    });
+  }
+  
 
 }
