e0bb538cdbf1b3f98c75432010a41ca2a98f2f94
RFrankowski <r.franek93@gmail.com>
2018-05-22 15:02:56 +0200

    rozwiazanie kofliku

--- src/app/register/shared/rejestracja.model.ts
+++ src/app/register/shared/rejestracja.model.ts
@@ -1,14 +1,8 @@
 export class Rejestracja {
-<<<<<<< HEAD
     public id?: string;
     public login?: string;
     public email?: string;
     public haslo?: string;
-=======
-    login?:string;
-    email?:string;
-    haslo?:string;
->>>>>>> 4c492b79f3a051760cf706f29f99f71c4eacf3e1
 
 
 }
